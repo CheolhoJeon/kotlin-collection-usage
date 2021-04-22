5f91d90f8413beaf0c47307906842ccd1f01068c
전철호 <cheolho@wisoft.io>
2021-04-22 15:36:33 +0900

    ktlin 적용을 위한 그레이들 플러그인 등록
    build.gradle -> build.gradle.kts
    add .editorconfig
    FoodToYouApplication.java -> FoodToYouApplication.kt
    
    Resolves: #5
    See also: #issueNo

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
 group = "me.jeoncheolho"
@@ -10,8 +11,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"

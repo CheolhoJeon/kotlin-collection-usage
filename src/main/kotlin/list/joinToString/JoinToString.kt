a7e301ed2fe44bcecbe9330787acf3cbeb19fa3f
전철호 <cheolho@wisoft.io>
2021-05-29 20:29:26 +0900

    Adding example of joinToString()

---
+++ src/main/kotlin/list/joinToString/JoinToString.kt
@@ -0,0 +1,24 @@
+package list.joinToString
+
+import atomictest.eq
+
+fun main() {
+    val list = listOf(9, 11, 23, 32)
+    list.joinToString(" ") { "[$it]" } eq
+        "[9] [11] [23] [32]"
+
+    // Default value of [prefix] and [postfix] is "":
+    list.joinToString(prefix = "[first] ", postfix = " [last]", separator = " ") { "[$it]" } eq
+        "[first] [9] [11] [23] [32] [last]"
+
+    // Default value of [separator] is ", ":
+    list.joinToString() { "$it" } eq
+        "9, 11, 23, 32"
+
+    // If number of element is bigger than [limit]
+    list.joinToString(limit = 3, truncated = "...") eq
+        "9, 11, 23, ..."
+
+    // You don't need to use joinTo()
+    // because StringBuffer is used in joinToString():
+}
